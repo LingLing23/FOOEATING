3222fb1e837c6bf9d77b4c2421eba9761114c930
hayun <hs183000@naver.com>
2023-05-05 18:18:06 +0900

    점주전환 페이지 스텝1,2,3,4 큰틀잡

---
+++ src/main/java/com/fooeating/javabean/JavaBeanpt3.java
@@ -0,0 +1,42 @@
+package com.fooeating.javabean;
+
+
+public class JavaBeanpt3 {
+
+	private String name;
+	private String descriptions;
+	private String price;
+	private String file;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescriptions() {
+		return descriptions;
+	}
+	public void setDescriptions(String descriptions) {
+		this.descriptions = descriptions;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	@Override
+	public String toString() {
+		return "JavaBeanpt3 [name=" + name + ", descriptions=" + descriptions + ", price=" + price + ", file=" + file
+				+ "]";
+	}
+
+	
+ 
+}
